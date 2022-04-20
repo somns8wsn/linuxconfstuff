1436f10d10d482bd919f33a07658f09b77884750
somns8wsn <tvsp6q@bk.ru>
2022-04-21 02:46:13 +0300

    1

--- .config/qtile/config.py
+++ .config/qtile/config.py
@@ -29,7 +29,7 @@ keys = [
     Key([mod, "control"], "k", lazy.layout.grow_up(), desc="Grow window up"),
     Key([mod], "n", lazy.layout.normalize(), desc="Reset all window sizes"),
 
-    
+
 
     # Toggle between split and unsplit sides of stack.
     # Split = all windows displayed
